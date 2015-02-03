680d323070614b3054bdbaf05ff9335ef3eff6d2
Rahul Johari <rjohari23@users.noreply.github.com>
2015-02-04 01:05:20 +0530

    Update solution.cpp

--- Are they the same/solution.cpp
+++ Are they the same/solution.cpp
@@ -1,6 +1,6 @@
-/* this solution will fetch you a score of 89.27 */
+/* this solution will fetch you a score of 89.53 */
 /* you have minimise the code length */
 /* if anybody has a better solution, you can show us */
 
 #include<bits/stdc++.h>
-using namespace std;main(){string s,S;cin>>s>>S;sort(s.begin(),s.end());sort(S.begin(),S.end());if(s==S)cout<<"YES\n";else cout<<"NO\n";}
+using namespace std;main(){string s,S;cin>>s>>S;sort(s.begin(),s.end());sort(S.begin(),S.end());if(s==S)cout<<"YES";else cout<<"NO";}
