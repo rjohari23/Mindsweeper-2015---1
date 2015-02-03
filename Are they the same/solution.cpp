cf592097b0488c72abae3a0a5e8433cd6988129d
Rahul Johari <rjohari23@users.noreply.github.com>
2015-02-04 00:59:07 +0530

    Update solution.cpp

--- Are they the same/solution.cpp
+++ Are they the same/solution.cpp
@@ -1,6 +1,6 @@
-/* this solution will fetch you a score of 88.87 */
+/* this solution will fetch you a score of 89.27 */
 /* you have minimise the code length */
 /* if anybody has a better solution, you can show us */
 
 #include<bits/stdc++.h>
-using namespace std;main(){char s[99],S[99];cin>>s>>S;sort(s,s+strlen(s));sort(S,S+strlen(S));if(!strcmp(s,S))cout<<"YES\n";else cout<<"NO\n";}
+using namespace std;main(){string s,S;cin>>s>>S;sort(s.begin(),s.end());sort(S.begin(),S.end());if(s==S)cout<<"YES\n";else cout<<"NO\n";}
